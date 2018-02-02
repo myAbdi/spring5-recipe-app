89eef4f0b5c8957778adb9c1364393093ac4ef58
myAbdi <abdieed@gmail.com>
2018-02-02 12:55:12 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package guru.springframework.controllers;
+
+import org.hibernate.boot.jaxb.SourceType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index"})
+    public String getIndexPage(){
+        System.out.println("Some out put......");
+        return "index";
+    }
+}
